ba104d2081c8e73946277a590f5a4ccee113974b
Per Fagrell <mango@spotify.com>
2016-04-20 16:14:05 +0200

    don't copy and paste

--- laptop.pp
+++ laptop.pp
@@ -185,7 +185,6 @@ exec { "accept-oracle-license":
 exec { "see-oracle-license":
     command => "echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections",
     path    => "/bin/",
-    require => Exec[see-oracle-license]
 }
 
 ### Fuck unity hidey-disappeary scrollbars!!!! :D ###
